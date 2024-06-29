76c2ac1574ef656602f0bffdb7bc00839d5d5e96
Shrivarshini-10 <112470041+Shrivarshini-10@users.noreply.github.com>
2024-06-29 15:36:10 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,9 @@
+//connection
+const mongoose= require('mongoose');
+mongoose.connect('mongodb://localhost:27017/ecommerce',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+module.exports.mongoose;
